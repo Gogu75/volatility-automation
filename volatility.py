e914eb1e0b766c464a80185698c17025cefbba76
Gogu75 <37547971+Gogu75@users.noreply.github.com>
2020-06-16 17:40:07 +0200

    Update volatility.py

--- volatility.py
+++ volatility.py
@@ -12,8 +12,6 @@ os.system("volatility imageinfo -f " + sys.argv[1])
 print("Entrez l'image du profil : ")
 profil = input()
 
-#profil = "Win2003SP0x86"
-
 #list of all volatility commands
 liste = ["atoms", "atomscan", "auditpol", "bioskbd", "cachedump", "callacks", "clipboard", "cmdline", "cmdscan", "netscan","dlllist", "driverirp", "drivermodule", "driverscan", "envars", "eventhooks", "filescan", "gahti", "gditimers", "gdt", "getservicesids", "getsids", "handles", "hashdump", "hibinfo", "hivelist", "idt", "iehistory", "joblinks", "kdbgscan", "kpcrscan", "ldrmodules", "lsadump", "machoinfo", "malfind", "mbrparser", "messagehooks", "mftparser", "modscan", "modules", "mutantscan", "objtypescan", "privs", "pslist", "psscan", "pstree", "psxview", "sessions", "shellbags", "shimcache", "sockets", "ssdt", "strings", "svcscan", "symlinkscan", "thrdscan", "threads", "timeliner", "timers", "unloadedmodules", "userassist", "userhandles", "vadinfo", "vadtree", "vadwalk", "windows", "wndscan"]
 
